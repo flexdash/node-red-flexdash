bb542797ffb7259c1dc56c680336b15793a30683
Thorsten von Eicken <tve@voneicken.com>
2022-07-19 13:21:11 -0700

    0.4.27; fixes prod proxy

--- flexdash-dashboard.js
+++ flexdash-dashboard.js
@@ -166,13 +166,13 @@ module.exports = function(RED) { try { // use try-catch to get stack backtrace o
             this.warn(`Cannot read ${paths.prodIndexHtml}: ${err}`)
             return res.status(500).send(`Cannot read index.html`)
           }
-          let flexdash_options = {
-            sio: `window.location.origin+"${ioPath}"`,
+          let flexdash_options = JSON.stringify({
+            sio: `window.location.origin+'${ioPath}'`,
             title: this.name,
             no_add_delete: true,
             no_demo: true,
-          }
-          res.send(data.toString().replace('{}', JSON.stringify(flexdash_options)))
+          }).replace(/"(w[^"]*)"/, '$1')
+          res.send(data.toString().replace('{}', flexdash_options))
         })
       })
       app.use(path||'/', Express.static(paths.prodRoot, { extensions: ['html'] }))
