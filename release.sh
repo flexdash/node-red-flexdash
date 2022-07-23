e27aecb1d352fcab57aa76fc21d01ee918561c90
Thorsten von Eicken <tve@voneicken.com>
2022-07-22 17:29:53 -0700

    version v0.4.40

--- release.sh
+++ release.sh
@@ -1,4 +1,25 @@
 #! /bin/bash -ex
+
+# read commandline options
+RELEASE=0
+while getopts ":h" opt; do
+  case $opt in
+    h)
+      echo "Usage: $0 [-h] [-r]"
+      echo " -h show this help message and exit"
+      echo " -r release the version"
+      exit 0
+      ;;
+    r)
+      RELEASE=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 v=$(npm version --no-git-tag-version patch)
 (cd plugin; npm version --no-git-tag-version $v)
 vmm=${v%.*}
@@ -6,5 +27,9 @@ vmm=${vmm#v}
 ./bundle.sh $vmm
 git commit -a -m "version $v"
 git push
-(cd plugin; npm publish)
+( cd plugin;
+  npm publish --tag dev;
+  if [[ $RELEASE == 1 ]]; then npm publish --tag latest; fi
+)
 npm publish
+if [[ $RELEASE == 1 ]]; then npm publish --tag latest; fi
